f98a39e34e951e34d9139da80272889c7ca929ed
lpfei <lsxinl@sina.com>
2019-08-01 16:56:45 +0800

    xss

---
+++ src/main/java/com/example/core/filter/xss/XssAndSqlHttpServletRequestWrapper.java
@@ -0,0 +1,43 @@
+package com.example.core.filter.xss;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * description:防止xss攻击
+ * Created by lpfei on 2019/8/1
+ */
+public class XssAndSqlHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    private HttpServletRequest request;
+
+    public XssAndSqlHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+        this.request = request;
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String value = request.getParameter(name);
+        if (!StringUtils.isEmpty(value)) {
+            value = StringEscapeUtils.escapeHtml4(value);
+        }
+        return value;
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] parameterValues = super.getParameterValues(name);
+        if (parameterValues == null) {
+            return null;
+        }
+        for (int i = 0; i < parameterValues.length; i++) {
+            String value = parameterValues[i];
+            parameterValues[i] = StringEscapeUtils.escapeHtml4(value);
+        }
+        return parameterValues;
+    }
+}
---
+++ src/main/java/com/example/core/filter/xss/XssStringJsonSerializer.java
@@ -0,0 +1,28 @@
+package com.example.core.filter.xss;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.io.IOException;
+
+/**
+ * description:
+ * Created by lpfei on 2019/8/1
+ */
+public class XssStringJsonSerializer extends JsonSerializer<String> {
+    @Override
+    public Class<String> handledType() {
+        return String.class;
+    }
+
+    @Override
+    public void serialize(String value, JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+        if (value != null) {
+            String encodedValue = StringEscapeUtils.escapeHtml4(value);
+            jsonGenerator.writeString(encodedValue);
+        }
+    }
+}
