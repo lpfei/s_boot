6cbe3b22df277633d67958f2a0e8eee5ae1e6849
lpfei <lsxinl@sina.com>
2019-08-15 10:41:09 +0800

    1

--- src/main/java/com/example/core/support/mybatis/CodeGenerator.java
+++ src/main/java/com/example/core/support/mybatis/CodeGenerator.java
@@ -56,6 +56,12 @@ public class CodeGenerator {
         //是否覆蓋已有文件 默认值：false
         gc.setFileOverride(true);
         gc.setSwagger2(false); //实体属性 Swagger2 注解
+        // .setEntityName("%sEntity");
+        // .setMapperName("%sDao")
+        // .setXmlName("%sDao")
+        // .setServiceName("MP%sService")
+        // .setServiceImplName("%sServiceDiy")
+        // .setControllerName("%sAction")
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -119,6 +125,28 @@ public class CodeGenerator {
         strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
+
+        // 自定义 mapper 父类
+        // .setSuperMapperClass("com.baomidou.demo.TestMapper")
+        // 自定义 service 父类
+        // .setSuperServiceClass("com.baomidou.demo.TestService")
+        // 自定义 service 实现类父类
+        // .setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl")
+        // 自定义 controller 父类
+        // .setSuperControllerClass("com.baomidou.demo.TestController")
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // .setEntityColumnConstant(true)
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        // .setEntityBuilderModel(true)
+        // 【实体】是否为lombok模型（默认 false）<a href="https://projectlombok.org/">document</a>
+        // .setEntityLombokModel(true)
+        // Boolean类型字段是否移除is前缀处理
+        // .setEntityBooleanColumnRemoveIsPrefix(true)
+        // .setRestControllerStyle(true)
+        // .setControllerMappingHyphenStyle(true)
+
         mpg.setStrategy(strategy);
 
         //界面引擎
