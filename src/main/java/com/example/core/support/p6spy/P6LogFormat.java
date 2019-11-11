cac4f2fd3dc81925aa8e688bbde59822fdc80e67
lsxinl@sina.com <lsxinl@sina.com>
2019-11-11 10:58:32 +0800

    p6spy sql 监控版本更新

--- src/main/java/com/example/core/support/p6spy/P6LogFormat.java
+++ src/main/java/com/example/core/support/p6spy/P6LogFormat.java
@@ -1,21 +1,20 @@
 package com.example.core.support.p6spy;
 
-import com.p6spy.engine.spy.appender.SingleLineFormat;
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * description:
+ * description:sql 监控
  * Created by lpfei on 2019/8/2
  */
-public class P6LogFormat extends SingleLineFormat {
+public class P6LogFormat implements MessageFormattingStrategy {
 
     private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
 
     @Override
-    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql) {
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String a) {
         return !"".equals(sql.trim()) ? this.format.format(new Date()) + " | took " + elapsed + "ms | " + category + " | connection " + connectionId + "\n " + sql + ";" : "";
     }
-
 }
